5e3d4b0ebe2cb56dec93c0ae2c81684ca19a4a7e
stevens1434 <stevens1434@gmail.com>
2017-10-20 13:16:42 -0700

    checkout works again

--- src/Checkout.js
+++ src/Checkout.js
@@ -12,39 +12,53 @@ class Checkout extends Component {
 
   handleChange(e) {
     console.log("handleChange() in Core.js: ", e);
-    //setstate of selected item, possibly at item like data-id to grab array[i] info
+    //use if we want to do something with the information after checkout
   }
 
   render() {
-    if (this.props.checkoutItems === undefined) {
-      return([
-      <h1>You have not placed any items in your cart</h1>
-      ])
-    } else {
-    var mappedCheckout = this.props.checkoutItems.map((checkout, index) => {
+    // if (this.props.checkoutItems === undefined) {
+    //   return([
+    //   <h1>You have not placed any items in your cart</h1>
+    //   ])
+    // } else {
+
+    const flat = this.props.checkoutItem.reduce((total, amount) => {
+      return total.concat(amount);
+    }, []);
+    const flatter = flat.reduce((total, amount) => {
+      return total.concat(amount);
+    }, []);
+    console.log(flat);
+    console.log(flatter);
+    var mappedCheckout = flatter.map((checkout, index) => {
       return([
           // <div className="core" key={core.call}>{core}</div>
-        <p href="/perif" key={index} data-key={index} onClick={this.handleChange}>{checkout.name} {checkout.price} {checkout.description}</p>
+        <p href="/perif" key={index} data-key={index} onClick={this.handleChange}>
+              <p>{checkout.name}</p>
+              {checkout.price}
+              {checkout.description}</p>
       ])
     })
-  }
+    console.log(mappedCheckout)
+  // }
 
     return (
       <div>
         <div className="container">
-          <img src="http://www.xoticpc.com/media/wysiwyg/landingpages/store_fronts/xpc-desktops/gx12-destroyer-a.png" />
-          <div className="component">
-            <h1>Other</h1>
-          </div>
-          <div className="mainDesc">
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-          </div>
-          <ul>
+          <form>
+          <input className="checkout-input" type="text"></input>
+          <input className="checkout-input" type="text"></input>
+          <input className="checkout-input" type="text"></input>
+          <input className="checkout-input" type="text"></input>
+          <a className="checkout-button" href="/checkout">Checkout</a>
+          </form>
+          <ul className="checkout-items">
             {mappedCheckout}
           </ul>
         </div>
       </div>
     )
+
   }
   }
 
