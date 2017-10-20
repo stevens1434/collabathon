402fc8360c8e431fa113cf69d5920fcb021fc6a4
erikr93 <erikr93@gmail.com>
2017-10-19 17:13:21 -0700

    Routes routed and links linking

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import Core from './Core';
+import Perif from './Perif';
+import Checkout from './Checkout';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
-    return (
+    return(
+    <Router>
       <div className="App">
-
+        <nav>
+        <Link to='/core'>Core</Link>{' '}
+        <Link to='/perif'>Perif</Link>{' '}
+        <Link to='/checkout'>Checkout</Link>{' '}
+        </nav>
+        <Route path="/core" component={Core} />
+        <Route path="/perif" component={Perif} />
+        <Route path="/checkout" component={Checkout} />
       </div>
+      </Router>
     );
   }
 }
---
+++ src/Perif.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Perif extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Perif;
